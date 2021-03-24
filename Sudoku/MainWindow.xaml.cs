502c5733ab36498bbf7827e2c53c4fb1c11ed2a2
samivagyok-web <samivagyok@gmail.com>
2021-03-24 20:27:59 +0200

    Basic UI

---
+++ Sudoku/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace Sudoku
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void NumberValidation(object sender, TextCompositionEventArgs e)
+        {
+            Regex pattern = new Regex(@"[^0-9]+");
+            e.Handled = pattern.IsMatch(e.Text);
+        }
+    }
+}
